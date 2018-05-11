1b4b126dbc5397e2cacc058e42307aa257b6a94c
SimonGreenhill <simon@simon.net.nz>
2018-05-11 13:49:46 +0200

    adding info

--- test.py
+++ test.py
@@ -52,6 +52,23 @@ def test_parameters(cldf_dataset, cldf_logger):
 
 # "We identified 3,859 cognate sets across the n = 100 meanings..."
 def test_cognates(cldf_dataset, cldf_logger):
+    # Note: the excel file has a problem in that some items marked as cognate
+    # do NOT have lexemes, so the CLDF file will ignore this. 
+    # 
+    # H12_Civili_Congo        count-19    - no lexeme but cognate (singleton)
+    # F23_Sumbwa              count-26    - no lexeme but cognate (singleton)
+    # Mungaka_Grassfields     shame-1     - no lexeme but cognate (singleton)
+    # Bamun_Grassfields       shame-2     - no lexeme but cognate (singleton)
+    # Njen_Grassfields        shame-3     - no lexeme but cognate (singleton)
+    # Moghamo_Grassfields     shame-4     - no lexeme but cognate (singleton)
+    # JE43_Kuria              spear-15    - no lexeme but cognate
+    # D331_Bvanuma            belly-30    - no lexeme but cognate
+    # D333_Ndaaka             big-40      - no lexeme but cognate
+    # JE14_Rukiga             come-8      - no lexeme but cognate
+    # E56_Daiso               feather-42  - no lexeme but cognate
+    # 
+    # For some reason the items for singleton cognates are not included in the
+    # below count, meaning that we have an expected shortfall of 6 items:
     cogsets = {c['Cognateset_ID'] for c in cldf_dataset['CognateTable']}
-    assert len(cogsets) == 3859
+    assert len(cogsets) == 3859 - 6
 
